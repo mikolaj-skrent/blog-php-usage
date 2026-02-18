dde6a12be24c94458e602cbba01d6025a854771e
skrencikk <blablamiki4@gmail.com>
2026-02-18 15:21:08 +0100

    Added explenation comments

--- blog/rejestracja.php
+++ blog/rejestracja.php
@@ -1,15 +1,15 @@
 <?php 
 
-require_once("./connect.php");
+require_once("./connect.php"); // pobiera polecenie z pliku connect.php
 
 
 
 $username = $_POST["new-username"];
 $password = $_POST["new-password"];
-$rePass = $_POST["new-re-password"];
+$rePass = $_POST["new-re-password"];  // 7-9 deklaracja zmiennych
 
 
-if ($password == $rePass) {
+if ($password == $rePass) { // sprawdza czy string w password i rePass są takie same, jeżeli tak przepuszcza dalej i rejestruje użytkownika w bazie danych - w innym przypadku zwraca komunikat błędu użytkownika
     $_SESSION["msg"] = "Udało się zarejestrować!";
 
 $sql = "INSERT INTO `users` (`id`, `login`, `password`, `nickname`)
@@ -21,11 +21,12 @@ $sql = "INSERT INTO `users` (`id`, `login`, `password`, `nickname`)
     $_SESSION["msg"] = "Podano różne hasła.";
 }
 
-header("Location: /index.php/");
+header("Location: /index.php/");  // przekierowuje na daną stronę
 
 
 
-$condom -> close();
+$condom -> close(); // kończy połączenie connect DOM
+
 
 
 

e873d9b84fba365c0e76d0dfa5095c63ff3203d3
skrencikk <blablamiki4@gmail.com>
2026-02-18 15:18:31 +0100

    Added explenation comments

--- blog/logowanie.php
+++ blog/logowanie.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
-require_once("./connect.php");
+require_once("./connect.php");  // linie 2-3 zaczyna sesję i pobiera polecenie z pliku connect.php
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+$username = $_POST["username"]; 
+$password = $_POST["password"]; // ustawia zmienne
 
-$sql = ("SELECT * FROM users WHERE login='$username' AND password='$password'");
+$sql = ("SELECT * FROM users WHERE login='$username' AND password='$password'"); // deklarujemy polecenie zmiennej sql (czym jest)
 
-$result = mysqli_query($condom, $sql);
+$result = mysqli_query($condom, $sql); // sprawdza czy użytkownik z takimi danymi istnieje istnieje
 
-if(mysqli_num_rows($result)) {
+if(mysqli_num_rows($result)) { // if sprawdza czy wpisane dane logowania są poprawne
 $_SESSION["msg"] = "Podane nieprawidłowe dane. <br> Nie przejdziesz dalej!";
 header("Location: /index.php/");
 } else {
-    header("Location: s_blog-article.html");
+    header("Location: s_blog-article.html"); // header("Location ...   - przekierowuje spowrotem na daną stronę (w tym przypadku na s_blog-article.html)
 }
 
 
@@ -22,4 +22,5 @@ header("Location: /index.php/");
 
 
  
+
 ?>

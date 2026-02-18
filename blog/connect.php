e3eccaadfd977fa5845dd12f5a2ad9fdddd05e67
skrencikk <blablamiki4@gmail.com>
2026-02-18 15:11:31 +0100

    Added explenation comments

--- blog/connect.php
+++ blog/connect.php
@@ -1,11 +1,12 @@
 <?php
 
-$condom = mysqli_connect("localhost", "root", "", "blog");
-if($condom -> errno) {
-    echo "Nie połączono z bazą danych.";
+$condom = mysqli_connect("localhost", "root", "", "blog"); // negocjuje połączenie z bazą danych
+if($condom -> errno) { // errno zwraca numer błędu, a error zwraca jego opis
+    echo "Nie połączono z bazą danych."; // wiadomość zwrotna
     exit;
     } else {
-    echo "Połączono z bazą danyh." . "<br>";
+    echo "Połączono z bazą danyh." . "<br>"; // w innym (połącznie z baza danych) przypadku zwraca wiadomość o sukcesie
 }
+
 
 ?>

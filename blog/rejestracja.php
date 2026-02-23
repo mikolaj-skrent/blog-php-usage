1e4536c8f87894c7e79a65510814ab824447eeef
skrencikk <blablamiki4@gmail.com>
2026-02-23 12:37:33 +0100

    Refactor user registration logic and add checks

--- blog/rejestracja.php
+++ blog/rejestracja.php
@@ -1,33 +1,46 @@
-<?php 
-
+<?php
+session_start();
 require_once("./connect.php"); // pobiera polecenie z pliku connect.php
 
-
+echo $username = $_POST["new-username"];
+echo "<br>";
+echo $password = $_POST["new-password"];
+echo "<br>";
+echo $rePass = $_POST["new-re-password"];
+echo "<br><hr><br>";
 
 $username = $_POST["new-username"];
 $password = $_POST["new-password"];
-$rePass = $_POST["new-re-password"];  // 7-9 deklaracja zmiennych
+$rePass = $_POST["new-re-password"];  // deklaracja zmiennych
 
-
-if ($password == $rePass) { // sprawdza czy string w password i rePass są takie same, jeżeli tak przepuszcza dalej i rejestruje użytkownika w bazie danych - w innym przypadku zwraca komunikat błędu użytkownika
-    $_SESSION["msg"] = "Udało się zarejestrować!";
-
-$sql = "INSERT INTO `users` (`id`, `login`, `password`, `nickname`)
- VALUES (NULL, '$username', '$password', '$rePass')";
-
- mysqli_query($condom, $sql);
-
-} else {
+if ($password != $rePass) {
     $_SESSION["msg"] = "Podano różne hasła.";
 }
 
-header("Location: /index.php/");  // przekierowuje na daną stronę
+
+// sprawdza czy jest juz taki login w bazie 22 - 30
+$checkUserQuery = "SELECT id FROM `users` WHERE `login` = '$username'";
+$checkResult = mysqli_query($condom, $checkUserQuery);
+
+if (mysqli_num_rows($checkResult) > 0) {
+    // Jeśli licznik wierszy jest większy niż 0, znaczy że login zajęty
+    $_SESSION["msg"] = "Taki użytkownik już istnieje!";
+    header("Location: index.php");
+    exit();
+}
+
+$sql = "INSERT INTO `users` (`id`, `login`, `password`, `nickname`)
+  VALUES (NULL, '$username', '$password', '$rePass')";
+
+if ($password == $rePass) { // sprawdza czy string w password i rePass są takie same, jeżeli tak przepuszcza dalej i rejestruje użytkownika w bazie danych - w innym przypadku zwraca komunikat błędu użytkownika
+    $_SESSION["msg"] = "Udało się zarejestrować!";
+    mysqli_query($condom, $sql);
+}
 
 
 
-$condom -> close(); // kończy połączenie connect DOM
 
 
+$condom->close(); // kończy połączenie connect DOM
 
-
-?>
+header("Location: index.php");  // przekierowuje na daną stronę

5ee1064df289b5893dc72cc1cfb0a2863f0615f6
skrencikk <blablamiki4@gmail.com>
2026-02-23 12:37:47 +0100

    Refactor login logic and update redirect path

--- blog/logowanie.php
+++ blog/logowanie.php
@@ -2,7 +2,7 @@
 session_start();
 require_once("./connect.php");  // linie 2-3 zaczyna sesję i pobiera polecenie z pliku connect.php
 
-$username = $_POST["username"]; 
+$username = $_POST["username"];
 $password = $_POST["password"]; // ustawia zmienne
 
 $sql = ("SELECT * FROM users WHERE login='$username' AND password='$password'"); // deklarujemy polecenie zmiennej sql (czym jest)
@@ -10,19 +10,9 @@ $sql = ("SELECT * FROM users WHERE login='$username' AND password='$password'");
 $result = mysqli_query($condom, $sql); // sprawdza czy użytkownik z takimi danymi istnieje istnieje
 
 if (mysqli_num_rows($result)) { // if sprawdza czy wpisane dane logowania są poprawne
-    header("Location: /blog/s_blog-article.html"); // header("Location ...   - przekierowuje spowrotem na daną stronę (w tym przypadku na s_blog-article.html)
+    $_SESSION["user"] = $username;
+    header("Location: /blog/blog-article.php"); // przekierowuje na daną stronę
 } else {
     $_SESSION["msg"] = "Podane nieprawidłowe dane. <br> Nie przejdziesz dalej!";
     header("Location: /blog/");
 }
-
-
-
-
-
-
-
- 
-
-?>
-
